0c81e46a2cce19ea1f77a4b7afb486e8443ee1aa
kshitijhadke26 <kshitijhadke26@gmail.com>
2025-05-17 15:05:11 +0530

    verifying hashed password

--- query/loginExe.php
+++ query/loginExe.php
@@ -1,30 +1,30 @@
 <?php 
 session_start();
- include("../conn.php");
- 
+include("../conn.php");
 
 extract($_POST);
 
-$selAcc = $conn->query("SELECT * FROM examinee_tbl WHERE exmne_email='$username' AND exmne_password='$pass'  ");
+// Step 1: Fetch user by email only (no password in query)
+$selAcc = $conn->prepare("SELECT * FROM examinee_tbl WHERE exmne_email = ?");
+$selAcc->execute([$username]);
 $selAccRow = $selAcc->fetch(PDO::FETCH_ASSOC);
 
-
-if($selAcc->rowCount() > 0)
-{
-  $_SESSION['examineeSession'] = array(
-  	 'exmne_id' => $selAccRow['exmne_id'],
-  	 'examineenakalogin' => true
-  );
-  $res = array("res" => "success");
-
+// Step 2: Verify password using password_verify()
+if ($selAcc && $selAcc->rowCount() > 0) {
+    if (password_verify($pass, $selAccRow['exmne_password'])) {
+        $_SESSION['examineeSession'] = array(
+            'exmne_id' => $selAccRow['exmne_id'],
+            'examineenakalogin' => true
+        );
+        $res = array("res" => "success");
+    } else {
+        // Password doesn't match
+        $res = array("res" => "invalid");
+    }
+} else {
+    // Email doesn't exist
+    $res = array("res" => "invalid");
 }
-else
-{
-  $res = array("res" => "invalid");
-}
 
-
-
-
- echo json_encode($res);
- ?>
+echo json_encode($res);
+?>
