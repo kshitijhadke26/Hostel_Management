8e6b50262c57db57f72f18fc07c909b99957c34e
kshitijhadke26 <kshitijhadke26@gmail.com>
2025-05-17 14:52:16 +0530

    total examinee count issue resolve

--- adminpanel/admin/query/countData.php
+++ adminpanel/admin/query/countData.php
@@ -3,11 +3,11 @@
 // Count All Course
 $selCourse = $conn->query("SELECT COUNT(cou_id) as totCourse FROM course_tbl ")->fetch(PDO::FETCH_ASSOC);
 
-
 // Count All Exam
 $selExam = $conn->query("SELECT COUNT(ex_id) as totExam FROM exam_tbl ")->fetch(PDO::FETCH_ASSOC);
 
-
+// Count All Examinee
+$selExaminee = $conn->query("SELECT COUNT(exmne_id) as totExaminee FROM examinee_tbl ")->fetch(PDO::FETCH_ASSOC);
 
 
  ?>
